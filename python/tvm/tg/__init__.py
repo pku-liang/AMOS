e27a5895b5f07328ff472faaead84d4a17ff9c75
KnowingNothing <zhengsizeMax@outlook.com>
2020-07-13 20:09:27 +0800

    add tag gen

--- python/tvm/tg/__init__.py
+++ python/tvm/tg/__init__.py
@@ -4,8 +4,6 @@
 
 from .autodiff import gradient
 from .autodiff import expr_equal, grad_op
-from .graph import get_batch_like_dim, find_axis_in, count_operation, count_input_occur, \
-                      subgraph_partition, make_tir_graph_inference, make_tir_multi_graph, \
-                      make_tir_graph_training
+from .graph import *
 from .auto_schedule import *
 from .runtime import *
