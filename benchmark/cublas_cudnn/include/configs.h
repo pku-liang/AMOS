afe48e733c34572d3dd455bd2cb6a8f8c5bf238c
Anjiang-Wei <1020681930@qq.com>
2020-11-25 23:03:31 +0800

    Add depth-wise convolution using mobilev2_shapes_b1 config

--- benchmark/cublas_cudnn/include/configs.h
+++ benchmark/cublas_cudnn/include/configs.h
@@ -2,8 +2,9 @@
 #include <tuple>
 
 
-// Vector saves w, h, c, n, k, filter_w(s), filter_h(r), pad_w, pad_h, wstride,
-// hstride
+// Vector saves w, h, c, n,
+//              k, filter_w(s), filter_h(r), pad_w,
+//              pad_h, wstride, hstride
 std::vector<std::tuple<unsigned int, unsigned int, unsigned int, unsigned int,
                        unsigned int, unsigned int, unsigned int, unsigned int,
                        unsigned int, unsigned int, unsigned int>>
@@ -23,4 +24,24 @@ std::vector<std::tuple<unsigned int, unsigned int, unsigned int, unsigned int,
         std::make_tuple(14, 14, 1024, 1, 1024, 3, 3, 1, 1, 1, 1),
         std::make_tuple(14, 14, 1024, 1, 1024, 3, 3, 1, 1, 2, 2),
         std::make_tuple(7, 7, 1024, 1, 1024, 3, 3, 1, 1, 1, 1)
+};
+
+
+
+// Vector saves w, h, c, n,
+//              k, filter_w(s), filter_h(r), pad_w,
+//              pad_h, wstride, hstride, groupcnt
+std::vector<std::tuple<unsigned int, unsigned int, unsigned int, unsigned int,
+                       unsigned int, unsigned int, unsigned int, unsigned int,
+                       unsigned int, unsigned int, unsigned int, unsigned int>>
+    depth_wise = { 
+		  // modify from yolo_v1 to satisfy: groupcnt == c, and k is multiple of c
+		  //                w,   h,  c,  n,     k, fw,  fh, pw, ph, ws, hs, groupcnt
+		  std::make_tuple(112, 112, 32,  1,  32*1,  3,   3,  1,  1,  1,  1,  32),
+		  std::make_tuple(112, 112, 16,  1,  16*6,  3,   3,  1,  1,  2,  2,  16),
+		  std::make_tuple( 56,  56, 24,  1,  24*6,  3,   3,  1,  1,  2,  2,  24),
+		  std::make_tuple( 28,  28, 32,  1,  32*6,  3,   3,  1,  1,  2,  2,  32),
+		  std::make_tuple( 14,  14, 64,  1,  64*6,  3,   3,  1,  1,  1,  1,  64),
+		  std::make_tuple( 14,  14, 96,  1,  96*6,  3,   3,  1,  1,  2,  2,  96),
+		  std::make_tuple(  7,   7,160,  1, 160*6,  3,   3,  1,  1,  1,  1, 160)
 };
