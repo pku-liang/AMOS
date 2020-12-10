364eb9f79f24f75ce2ff69e2775c38e2654867c9
Bingyang Wu <steven@scccc.pkusc.org>
2020-12-10 13:54:09 +0800

    check gemm

--- benchmark/cublas_cudnn/src/cuda_gemm_bench.cu
+++ benchmark/cublas_cudnn/src/cuda_gemm_bench.cu
@@ -7,6 +7,7 @@
 #include <stdexcept>
 #include <tuple>
 #include <vector>
+#include <algorithm>
 
 #include <cublas_v2.h>
 #include <cuda.h>
@@ -195,6 +196,90 @@ int time_gemm(Tensor<T1> A, Tensor<T1> B, Tensor<T2> C, bool a_t, bool b_t,
       numRepeats);
 }
 
+template <typename T1, typename T2>
+bool check_gemm(Tensor<T1> A, Tensor<T1> B, Tensor<T2> C, bool a_t, bool b_t,
+              cublasHandle_t cublas_handle, bool use_tensor_core) {
+
+  const float alpha = 1.f;
+  const float beta = 1.f;
+
+  int m = C.dims()[0];
+  int k = a_t ? A.dims()[0] : A.dims()[1];
+  int n = C.dims()[1];
+
+  int lda = A.dims()[0], ldb = B.dims()[0], ldc = C.dims()[0];
+  T1 *A_host, *B_host;
+  std::vector<T2> C_host(n * ldc), C_reference(n * ldc);
+
+  A_host = (T1 *)malloc(A.size() * sizeof(T1));
+  cudaMemcpy(A_host, A.begin(), A.size() * sizeof(T1), cudaMemcpyDeviceToHost);
+  B_host = (T1 *)malloc(B.size() * sizeof(T1));
+  cudaMemcpy(B_host, B.begin(), B.size() * sizeof(T1), cudaMemcpyDeviceToHost);
+  cudaMemcpy(C_reference.data(), C.begin(), C.size() * sizeof(T2), cudaMemcpyDeviceToHost);
+
+  int numRepeats = 6;
+  cublasStatus_t stat;
+
+  cudaDataType_t A_type = CUDA_R_32F;
+  cudaDataType_t B_type = CUDA_R_32F;
+  cudaDataType_t C_type = CUDA_R_32F;
+  cudaDataType_t compute_type = CUDA_R_32F;
+  cublasGemmAlgo_t algo;
+
+  if (std::is_same<T1, uint16_t>::value) {
+    A_type = CUDA_R_16F;
+    B_type = CUDA_R_16F;
+    C_type = CUDA_R_16F;
+    compute_type = CUDA_R_16F;
+  }
+
+  if (std::is_same<T1, uint8_t>::value) {
+    A_type = CUDA_R_8I;
+    B_type = CUDA_R_8I;
+    C_type = CUDA_R_32I;
+    compute_type = CUDA_R_32I;
+  }
+
+  algo = use_tensor_core ? CUBLAS_GEMM_DFALT_TENSOR_OP : CUBLAS_GEMM_DFALT;
+
+  stat =
+      cublasGemmEx(cublas_handle, a_t ? CUBLAS_OP_T : CUBLAS_OP_N,
+                   b_t ? CUBLAS_OP_T : CUBLAS_OP_N, m, n, k, &alpha, A.begin(),
+                   A_type, A.dims()[0], B.begin(), B_type, B.dims()[0], &beta,
+                   C.begin(), C_type, C.dims()[0], compute_type, algo);
+  
+  cudaMemcpy(C_host.data(), C.begin(), C.size() * sizeof(T2), cudaMemcpyDeviceToHost);
+
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    throw std::runtime_error("sgemm failed");
+  }
+  
+  for (int i = 0; i < m; ++i)
+  for (int j = 0; j < n; ++j) {
+    T2 accumulator = 0;
+    for (int z = 0; z < k; ++z) {
+      accumulator += A_host[i * k + z] * B_host[z * n + j];
+    }
+    C_reference[i * n + j] = alpha * accumulator + beta * C_reference[i * n + j];
+  }
+
+  float rtol = 1e-2, atol = 1e-2;
+  for (int i = 0; i < m; ++i)
+  for (int j = 0; j < n; ++j) {
+    float C_diff = std::abs(C_host[i * n + j] - C_reference[i * n + j]);
+    float C_min = std::min(std::abs(C_host[i * n + j]), std::abs(C_reference[i * n + j]));
+    if (!(C_diff <= atol && C_diff / C_min <= rtol)) {
+      std::cout << "\n(" << i << "," << j << "): atol=" << C_diff << ", rtol=" << C_diff/C_min << std::endl;
+      std::cout << "-------" << std::endl;
+      std ::cout << A.dims()[0] << "," << A.dims()[1] << std::endl;
+      std ::cout << B.dims()[0] << "," << B.dims()[1] << std::endl;
+      std ::cout << C.dims()[0] << "," << C.dims()[1] << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
 int main(int argc, char **argv) {
   // Get Device Number
   int deviceCount = 0;
@@ -262,6 +347,7 @@ int main(int argc, char **argv) {
         std::cout << "CUBLAS math mode failed" << std::endl;
       }
 
+
       // fp32 benchmark
       {
         auto a = rand<float>({a_t ? k : m, a_t ? m : k}, curand_gen);
@@ -270,6 +356,11 @@ int main(int argc, char **argv) {
         time_ms =
             time_gemm<float, float>(a, b, c, a_t, b_t, cublas_handle, false);
         std::cout << "," << std::setprecision(6) << time_ms;
+        if (!check_gemm<float, float>(a, b, c, a_t, b_t, cublas_handle, false)) {
+          std::cout << "NO!!!!" << std::endl;
+          return 0;
+        }
+        else std::cout << "YES!!!" << std::endl;
       }
 
       // fp16 benchmark
