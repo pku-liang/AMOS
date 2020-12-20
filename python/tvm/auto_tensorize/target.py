045e03750e911d5583a64a499c52b3fe7bbb3f68
Light-of-Hers <724610746@qq.com>
2020-12-20 16:48:28 +0800

    schedule: add simple scheduling for mali gpu

--- python/tvm/auto_tensorize/target.py
+++ python/tvm/auto_tensorize/target.py
@@ -29,7 +29,7 @@ class CUDA(AcceleratorTarget):
 
     def get_shared_memory_bytes(self):
         return 2**16
-    
+
     def get_warp_size(self):
         return 32
 
@@ -41,3 +41,28 @@ class CUDA(AcceleratorTarget):
 
     def max_blocks(self):
         return 2**16
+
+
+class Mali(AcceleratorTarget):
+    # shared_mem, warp_size, regs_per_threads, max_threads, max_blocks
+    _arch_params = {
+        "g76": (0, 8, 255, 1024, 2 ** 16),
+    }
+
+    def __init__(self, arch="g76"):
+        self.arch = arch
+
+    def get_shared_memory_bytes(self):
+        return self._arch_params[self.arch][0]
+
+    def get_warp_size(self):
+        return self._arch_params[self.arch][1]
+
+    def get_register_bytes_per_thread(self):
+        return self._arch_params[self.arch][2]
+
+    def max_threads(self):
+        return self._arch_params[self.arch][3]
+
+    def max_blocks(self):
+        return self._arch_params[self.arch][4]
