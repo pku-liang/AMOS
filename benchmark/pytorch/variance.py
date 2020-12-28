7904e8aba4503870ff58dca3b4def1faa3e5c887
Anjiang-Wei <1020681930@qq.com>
2020-12-28 15:43:36 +0800

    Add variance pytorch benchmark

---
+++ benchmark/pytorch/variance.py
@@ -0,0 +1,96 @@
+import torch
+import numpy as np
+
+def mean_prof(N, C, H, W, dtype):
+  A_np = np.random.uniform(-10, 10, [N, C, H, W]).astype("float32")
+  B_np = np.random.uniform(-10, 10, [N, C, H, W]).astype("float32")
+
+
+  # What's supported by NVIDIA? Refer to https://docs.nvidia.com/cuda/ampere-tuning-guide/index.html
+
+  # What's supported by pytorch? I don't know
+  # Please sudo nvprof them!
+
+  torch.backends.cuda.matmul.allow_tf32 = False
+  torch.backends.cudnn.allow_tf32 = False
+
+  if dtype == "FP16": # HMMA-16, torch.float16 or torch.half
+    A_torch = torch.tensor(A_np).type(torch.float16).cuda()
+    B_torch = torch.tensor(B_np).type(torch.float16).cuda()
+  elif dtype == "BF16": # HMMA-16, only on NVIDIA A100, torch.bfloat16
+    A_torch = torch.tensor(A_np).type(torch.bfloat16).cuda()
+    B_torch = torch.tensor(B_np).type(torch.bfloat16).cuda()
+  elif dtype == "FP32":
+    A_torch = torch.tensor(A_np).type(torch.float32).cuda()
+    B_torch = torch.tensor(B_np).type(torch.float32).cuda()
+  elif dtype == "TF32": # HMMA-19, NVIDIA A100
+    # Please upgrade torch to 1.7; only supported on A100
+    # https://pytorch.org/docs/stable/notes/cuda.html#tf32-on-ampere
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+    A_torch = torch.tensor(A_np).type(torch.float32).cuda()
+    B_torch = torch.tensor(B_np).type(torch.float32).cuda()
+  elif dtype == "INT8": # IMMA, but pytorch has no support for INT8 GEMM
+    A_torch = torch.tensor(A_np).type(torch.int8).cuda()
+    B_torch = torch.tensor(B_np).type(torch.int8).cuda()
+  # Pytorch has no int4 type
+  elif dtype == "BOOL": # BMMA, but pytorch has no support for GEMM GEMM
+    A_torch = torch.tensor(A_np).type(torch.bool).cuda()
+    B_torch = torch.tensor(B_np).type(torch.bool).cuda()
+  elif dtype == "FP64": # DMMA(FP64), only supported on A100
+    A_torch = torch.tensor(A_np).type(torch.float64).cuda()
+    B_torch = torch.tensor(B_np).type(torch.float64).cuda()
+  else:
+    assert False, "wrong type: " + dtype
+
+  number = 10
+  repeats = 10
+
+  for i in range(repeats):
+      time_record = []
+      for j in range(number):
+          torch.cuda.synchronize()
+          start = torch.cuda.Event(enable_timing=True)
+          end = torch.cuda.Event(enable_timing=True)
+          start.record()
+
+          C_torch = torch.mean(torch.pow(A_torch-B_torch, 2), dim=[0, 2, 3], keepdim=False)
+
+          end.record()
+          torch.cuda.synchronize()
+          total = start.elapsed_time(end)
+          time_record.append(total)
+      if i == repeats - 1:
+        mean_cost = np.mean(time_record)
+  #print("conv2d, dtype = %s, A: %s, B: %s, C:%s" % (dtype, A_torch.dtype, B_torch.dtype, C_torch.dtype))
+  print(",".join(map(str, [N, C, H, W, dtype, mean_cost])))
+
+
+res18_shapes_b1 = [
+    # resnet-18
+    (1, 3, 224, 224, 64, 3, 7, 7, 1, 2, 3, 1, 1),  # conv1  0
+    (1, 64, 56, 56, 64, 64, 3, 3, 1, 1, 1, 1, 1),  # conv2   1
+    (1, 64, 56, 56, 64, 64, 1, 1, 1, 1, 0, 1, 1),  # conv3   2
+    (1, 64, 56, 56, 128, 64, 3, 3, 1, 2, 1, 1, 1),  # conv4   3
+    (1, 64, 56, 56, 128, 64, 1, 1, 1, 2, 0, 1, 1),  # conv5   4
+    (1, 128, 28, 28, 128, 128, 3, 3, 1, 1, 1, 1, 1),  # conv6   5
+    (1, 128, 28, 28, 256, 128, 3, 3, 1, 2, 1, 1, 1),  # conv7   6
+    (1, 128, 28, 28, 256, 128, 1, 1, 1, 2, 0, 1, 1),  # conv8   7
+    (1, 256, 14, 14, 256, 256, 3, 3, 1, 1, 1, 1, 1),  # conv9   8
+    (1, 256, 14, 14, 512, 256, 3, 3, 1, 2, 1, 1, 1),  # conv10  9
+    (1, 256, 14, 14, 512, 256, 1, 1, 1, 2, 0, 1, 1),  # conv11  10
+    (1, 512, 7, 7, 512, 512, 3, 3, 1, 1, 1, 1, 1),  # conv12  11
+]
+
+if __name__ == "__main__":
+    assert torch.backends.cudnn.is_available()
+    torch.backends.cudnn.enabled = True
+    beg = 0
+    num = len(res18_shapes_b1)
+    print("N, C, H, W, type, cost")
+    for dtype in ["FP16", "FP32", "TF32", "FP64", "BF16"]: # "INT8", "BOOL"
+        costs = []
+        for i, shape in enumerate(res18_shapes_b1[beg:beg+num]):
+          (N, C, H, W, _, _, _, _, _, _, _, _, _) = shape
+          mean_prof(N, C, H, W, dtype)
+    print("cudnn: %s" % ("enabled" if torch.backends.cudnn.enabled else "disabled"))
