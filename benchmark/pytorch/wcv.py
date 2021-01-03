7f303f7857de4675ea1e307fa82d889b9491be0e
Anjiang-Wei <1020681930@qq.com>
2021-01-03 22:45:35 +0800

    Add wcv pytorch benchmark

--- benchmark/pytorch/wcv.py
+++ benchmark/pytorch/wcv.py
@@ -1,10 +1,11 @@
 import torch
 import numpy as np
 
-def scan_prof(M, N, K, dtype):
-  A_np = np.random.uniform(-10, 10, [M, K]).astype("float32")
-  B_np = np.random.uniform(-10, 10, [K, N]).astype("float32")
-
+def weight_conv(N, I, O, groups, dtype):
+  channel_per_group = I // groups
+  # out_channel_per_group = O // groups
+  A_np = np.random.uniform(-10, 10, [N, I, 1, 1]).astype("float32")
+  B_np = np.random.uniform(-10, 10, [O, channel_per_group, 1, 1]).astype("float32")
 
   # What's supported by NVIDIA? Refer to https://docs.nvidia.com/cuda/ampere-tuning-guide/index.html
 
@@ -54,7 +55,8 @@ def scan_prof(M, N, K, dtype):
           end = torch.cuda.Event(enable_timing=True)
           start.record()
 
-          C_torch = torch.mm(A_torch, B_torch)
+          C_torch = torch.nn.functional.conv2d(A_torch, B_torch, bias=None,
+              stride=1, padding=0, groups=groups)
 
           end.record()
           torch.cuda.synchronize()
@@ -62,28 +64,39 @@ def scan_prof(M, N, K, dtype):
           time_record.append(total)
       if i == repeats - 1:
         mean_cost = np.mean(time_record)
-  #print("conv2d, dtype = %s, A: %s, B: %s, C:%s" % (dtype, A_torch.dtype, B_torch.dtype, C_torch.dtype))
-  print(",".join(map(str, [M, N, K, dtype, mean_cost])))
+  #print("wcv, dtype = %s, A: %s, B: %s, C:%s" % (dtype, A_torch.dtype, B_torch.dtype, C_torch.dtype))
+  print(",".join(map(str, [N, I, O, groups, dtype, mean_cost])))
 
 
-scan_shape = [
-  # M, N, K
-  (16, 512, 128),
-  (1024, 16, 256),
-  (256, 1024, 256),
-  (512, 256, 16),
-  (1024, 1024, 1024)
+
+# https://github.com/megvii-model/WeightNet/blob/master/shufflenet_v2.py
+# in_channels, out_channels, groups (ksize, stride, padding = 1, 1, 0)
+shuffle_v2_cfg = [
+    (24, 216, 24),
+    (48, 576, 48),
+    (56, 504, 56),
+    (112, 1008, 112),
+    (112, 1344, 112),
+    (112, 3136, 112),
+    (176, 4928, 176),
+    (224, 2016, 224),
+    (224, 12544, 224),
+    (448, 50176, 448),
 ]
+
 
 if __name__ == "__main__":
     assert torch.backends.cudnn.is_available()
     torch.backends.cudnn.enabled = True
+    batches = [2**i for i in range(1)]
     beg = 0
-    num = len(scan_shape)
-    print("M, N, K, type, cost")
+    num = len(shuffle_v2_cfg)
+    print("batch, in_channels, out_channels, groups, type, cost")
     for dtype in ["FP16", "FP32", "TF32", "FP64", "BF16"]: # "INT8", "BOOL"
-        costs = []
-        for i, shape in enumerate(scan_shape[beg:beg+num]):
-          (M, N, K) = shape
-          scan_prof(M, N, K, dtype)
+      for batch in batches:
+          costs = []
+          for i, shape in enumerate(shuffle_v2_cfg[beg:beg+num]):
+              (I, O, groups) = shape
+              N = batch
+              weight_conv(N, I, O, groups, dtype)
     print("cudnn: %s" % ("enabled" if torch.backends.cudnn.enabled else "disabled"))
